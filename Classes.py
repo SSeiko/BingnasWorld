c4226e03980767b62e0cee21b7f5308be9b20882
Harvey Brine <harvey.d.brine@gmail.com>
2022-09-09 15:06:14 +1000

    Starting classes and comments

---
+++ Classes.py
@@ -0,0 +1,14 @@
+class Organism():
+#contains behaviour all Organisms should have, Health, food, age, children, genes etc
+    return()
+
+class Food():
+# Contains behaviour all food should have, such as position, age, etc
+    return()
+
+class Map():
+# Holds list of coordinates and entities at each coordinate
+    def __init__():
+
+
+
